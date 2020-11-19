19a7a2ed3cf3b966dfbd6febc0d77daa41749b1b
Patryk Juraszek <pjuraszek@open.pl>
2020-11-19 11:29:43 +0100

    fix

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
 	console.log('Listening on port *: 3000');
 });
 
