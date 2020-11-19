936930841bb1fd60d04ee966df2b9fae775123a4
Patryk Juraszek <pjuraszek@open.pl>
2020-11-19 11:32:45 +0100

    fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
 	cors: {
-		origin: "https://pj-chat-app.netlify.app/",
+		origin: "https://pj-chat-app.netlify.app",
 		methods: ["GET", "POST"],
 		allowedHeaders: ["*"],
 		credentials: true
