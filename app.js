87b114ab6997b36bac67678b7769ebefb1fd3c1d
Patryk Juraszek <pjuraszek@open.pl>
2020-11-19 11:22:48 +0100

    fixed

--- app.js
+++ app.js
@@ -1,67 +1,81 @@
-const express = require('express');
-const socketIO = require('socket.io');
+// const express = require('express');
+// const socketIO = require('socket.io');
+//
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
+//
+// const server = express()
+// 	.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+// 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+//
+// const io = socketIO(server);
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+	cors: {
+		origin: "http://localhost:8080",
+		methods: ["GET", "POST"],
+		allowedHeaders: ["*"],
+		credentials: true
+	}
+});
 
-const server = express()
-	.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-	.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+});
+
+server.listen(3000, () => {
+	console.log('Listening on port *: 3000');
+});
 
 let channels = [];
 let members = [];
-let initialized = false;
-io.on("connection", socket => {
-	if (!initialized) {
-		socket.on("initChannels", channelsList => {
-			channels = channelsList;
-			console.log("Channels initialized");
-			socket.on("initMembers", membersList => {
-				members = membersList;
-				initialized = true;
-				console.log("Members initialized");
-			});
-		});
-	} else {
-		socket.on("userJoin", user => {
-			const index = members.findIndex(o => o.uid === user.uid);
+io.on("connection", (socket) => {
+	socket.on('joined', ({user, channel}) => {
+		const index = members.findIndex(o => o.uid === user.uid);
+		if(index !== -1) {
 			members[index].isActive = true;
-			console.log("User joined: " + user.nickName);
-			io.emit("updateMembers", members);
-		});
+		} else {
+			members.push({
+				...user,
+				isActive: true,
+				channel
+			})
+		}
+		io.emit('updateMembers', members);
+		console.log('Joined ' + user.nickname);
+	});
 
-		socket.on("userLeave", user => {
-			const index = members.findIndex(o => o.uid === user.uid);
+	socket.on('leave', (user) => {
+		const index = members.findIndex(o => o.uid === user.uid);
+		if(index !== -1) {
 			members[index].isActive = false;
-			console.log("User left: " + user.nickName);
-			io.emit("updateMembers", members);
-		});
+		}
+		io.emit('updateMembers', members);
+		console.log('Left ' + user.nickname);
+	});
 
-		socket.on("changeChannel", ({user, channel, messages}) => {
-			const index = channels.findIndex(o => o.slug === channel);
-			if(index !== -1) {
-				channels[index].messages = messages;
-			}
-			console.log("User: " + user.nickName + " has joined channel: " + channel);
-		});
+	socket.on("changeChannel", ({user, channel, messages}) => {
+		const index = channels.findIndex(o => o.slug === channel);
+		if(index !== -1) {
+			channels[index].messages = messages;
+		} else {
+			channels.push({
+				slug: channel,
+				messages,
+			})
+		}
 
-		socket.on("sendMessage", ({message, channel}) => {
-			const index = channels.findIndex(o => o.slug === channel);
-			channels[index].messages.push(message);
-			const messages = channels[index].messages;
-			console.log("New message: " + message.message + " on channel: " + channel);
-			io.emit("updateMessages", {channel, messages});
-		});
+		console.log("User: " + user.nickname + " has joined channel: " + channel);
+	});
 
-		socket.on("disconnect", () => {
-			socket.on("userLeave", user => {
-				const index = members.findIndex(o => o.uid === user.uid);
-				members[index].isActive = false;
-				console.log("User left: " + user.nickName);
-				io.emit("updateMembers", members);
-			});
-		});
-	}
+	socket.on("sendMessage", ({message, channel}) => {
+		const index = channels.findIndex(o => o.slug === channel);
+		channels[index].messages.push(message);
+		const messages = channels[index].messages;
+		console.log("New message: " + message.message + " on channel: " + channel);
+		io.emit("updateMessages", {channel, messages});
+	});
 });
