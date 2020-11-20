f518ed6d2a2e048445f37a9c9fa7d0ca8aebacfb
Patryk Juraszek <pjuraszek@open.pl>
2020-11-20 12:49:18 +0100

    fix

---
+++ README.md
@@ -0,0 +1,16 @@
+# Chat App Server
+This is server for [Chat App](https://pj-chat-app.netlify.app/) It is written in Express & Socket.IO.
+
+Demo version: [LINK](https://pj-chat-app.netlify.app/)
+
+## Running project
+
+### Install dependencies
+### `npm install`
+
+### Run locally
+### `npm run start`
+
+## Used technologies
+- Express JS
+- Socket.IO
--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const socketIO = require('socket.io');
-//
-// const PORT = process.env.PORT || 3000;
-// const INDEX = '/index.html';
-//
-// const server = express()
-// 	.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-// 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
-//
-// const io = socketIO(server);
-
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
